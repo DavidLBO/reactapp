b49262e7a9a1a6f6aecd9f346e32517288cd12ac
David Lopes <davidlopes009@gmail.com>
2022-06-15 14:22:48 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react'
+import './style.css';
+import {Card, CardProps} from "../../components/Card";
+
+type User = {
+  name: string;
+  avatar: string;
+}
+
+export function Home() {
+
+  const [studentName, setStudentName] = useState('');
+  const [students, setStudents] = useState<CardProps[]>([]);
+  const [user, setUser] = useState<User>({} as User);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]); 
+  }
+
+  useEffect(() => {
+    // corpo do useEffect (Só se pode usar Async nessa parte)
+    fetch('https://api.github.com/users/DavidLBO')
+    .then(response => response.json())
+    .then(data => {
+      setUser ({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+    })
+    .catch(error => console.error(error));
+  }, []);
+  return (
+    <div className='container'>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+      </header>
+      <input
+        type="text"
+        placeholder='Digite o nome...' 
+        onChange={e => setStudentName(e.target.value)}
+      />
+
+      <button type='button' onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+    {
+      students.map(student => (
+      <Card 
+        key={student.time}
+        name={student.name} 
+        time={student.time} 
+      />
+      ))
+    }
+      
+    </div>
+  )
+}
